59973aa6d80d61ecbe3971a8f5c3652fa4fe5d59
Jake Ke <jackieke724@hotmail.com>
2022-05-30 14:29:28 -0700

    dbg mem test

---
+++ verilog/dv/ann_dbg_mem/ann_dbg_mem.c
@@ -0,0 +1,277 @@
+/*
+ * SPDX-FileCopyrightText: 2020 Efabless Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+// This include is relative to $CARAVEL_PATH (see Makefile)
+#include <defs.h>
+#include <stub.c>
+
+/*
+	Wishbone Test:
+		- Configures MPRJ lower 8-IO pins as outputs
+		- Checks counter value through the wishbone port
+*/
+#define reg_mprj_cfg_mode                   (*(volatile uint32_t*)0x30000000)
+#define reg_mprj_cfg_debug                  (*(volatile uint32_t*)0x30000004)
+#define reg_mprj_cfg_done                   (*(volatile uint32_t*)0x30000008)
+#define reg_mprj_cfg_fsm_start              (*(volatile uint32_t*)0x3000000C)
+#define reg_mprj_cfg_fsm_done               (*(volatile uint32_t*)0x30000010)
+#define reg_mprj_cfg_load_done              (*(volatile uint32_t*)0x30000014)
+#define reg_mprj_cfg_send_done              (*(volatile uint32_t*)0x30000018)
+#define reg_mprj_cfg_cfg_done               (*(volatile uint32_t*)0x3000001C)
+#define reg_mprj_cfg_query                  ((volatile uint32_t*)0x30010000)
+#define reg_mprj_cfg_leaf                   ((volatile uint32_t*)0x30020000)
+#define reg_mprj_cfg_best                   ((volatile uint32_t*)0x30030000)
+#define reg_mprj_cfg_node                   ((volatile uint32_t*)0x30040000)
+
+
+void main()
+{
+
+	/* 
+	IO Control Registers
+	| DM     | VTRIP | SLOW  | AN_POL | AN_SEL | AN_EN | MOD_SEL | INP_DIS | HOLDH | OEB_N | MGMT_EN |
+	| 3-bits | 1-bit | 1-bit | 1-bit  | 1-bit  | 1-bit | 1-bit   | 1-bit   | 1-bit | 1-bit | 1-bit   |
+	Output: 0000_0110_0000_1110  (0x1808) = GPIO_MODE_USER_STD_OUTPUT
+	| DM     | VTRIP | SLOW  | AN_POL | AN_SEL | AN_EN | MOD_SEL | INP_DIS | HOLDH | OEB_N | MGMT_EN |
+	| 110    | 0     | 0     | 0      | 0      | 0     | 0       | 1       | 0     | 0     | 0       |
+	
+	 
+	Input: 0000_0001_0000_1111 (0x0402) = GPIO_MODE_USER_STD_INPUT_NOPULL
+	| DM     | VTRIP | SLOW  | AN_POL | AN_SEL | AN_EN | MOD_SEL | INP_DIS | HOLDH | OEB_N | MGMT_EN |
+	| 001    | 0     | 0     | 0      | 0      | 0     | 0       | 0       | 0     | 1     | 0       |
+	*/
+
+	/* Set up the housekeeping SPI to be connected internally so	*/
+	/* that external pin changes don't affect it.			*/
+
+    reg_spi_enable = 1;
+    reg_wb_enable = 1;
+	// reg_spimaster_config = 0xa002;	// Enable, prescaler = 2,
+                                        // connect to housekeeping SPI
+
+    // Connect the housekeeping SPI to the SPI master
+	// so that the CSB line is not left floating.  This allows
+	// all of the GPIO pins to be used for user functions.
+
+    reg_mprj_io_37 = GPIO_MODE_USER_STD_OUTPUT;
+    reg_mprj_io_36 = GPIO_MODE_USER_STD_OUTPUT;
+    reg_mprj_io_35 = GPIO_MODE_USER_STD_OUTPUT;
+    reg_mprj_io_34 = GPIO_MODE_USER_STD_OUTPUT;
+    reg_mprj_io_33 = GPIO_MODE_USER_STD_OUTPUT;
+    reg_mprj_io_32 = GPIO_MODE_USER_STD_OUTPUT;
+    reg_mprj_io_31 = GPIO_MODE_USER_STD_OUTPUT;
+    reg_mprj_io_30 = GPIO_MODE_USER_STD_OUTPUT;
+    reg_mprj_io_29 = GPIO_MODE_USER_STD_OUTPUT;
+    reg_mprj_io_28 = GPIO_MODE_USER_STD_OUTPUT;
+    reg_mprj_io_27 = GPIO_MODE_USER_STD_OUTPUT;
+    reg_mprj_io_26 = GPIO_MODE_USER_STD_OUTPUT;
+    reg_mprj_io_25 = GPIO_MODE_USER_STD_INPUT_NOPULL;
+    reg_mprj_io_24 = GPIO_MODE_USER_STD_OUTPUT;
+    reg_mprj_io_23 = GPIO_MODE_USER_STD_OUTPUT;
+    reg_mprj_io_22 = GPIO_MODE_USER_STD_OUTPUT;
+    reg_mprj_io_21 = GPIO_MODE_USER_STD_OUTPUT;
+    reg_mprj_io_20 = GPIO_MODE_USER_STD_OUTPUT;
+    reg_mprj_io_19 = GPIO_MODE_USER_STD_OUTPUT;
+    reg_mprj_io_18 = GPIO_MODE_USER_STD_OUTPUT;
+    reg_mprj_io_17 = GPIO_MODE_USER_STD_INPUT_NOPULL;
+    reg_mprj_io_16 = GPIO_MODE_USER_STD_INPUT_NOPULL;
+    reg_mprj_io_15 = GPIO_MODE_USER_STD_INPUT_NOPULL;
+    reg_mprj_io_14 = GPIO_MODE_USER_STD_INPUT_NOPULL;
+    reg_mprj_io_13 = GPIO_MODE_USER_STD_INPUT_NOPULL;
+    reg_mprj_io_12 = GPIO_MODE_USER_STD_OUTPUT; 
+    reg_mprj_io_11 = GPIO_MODE_USER_STD_INPUT_NOPULL;
+    reg_mprj_io_10 = GPIO_MODE_USER_STD_INPUT_NOPULL;
+    reg_mprj_io_9  = GPIO_MODE_USER_STD_INPUT_NOPULL;
+    reg_mprj_io_8  = GPIO_MODE_USER_STD_INPUT_NOPULL;
+    reg_mprj_io_7  = GPIO_MODE_USER_STD_INPUT_NOPULL;
+    reg_mprj_io_6  = GPIO_MODE_USER_STD_INPUT_NOPULL;
+    reg_mprj_io_5  = GPIO_MODE_USER_STD_INPUT_NOPULL;  
+    reg_mprj_io_4  = GPIO_MODE_USER_STD_INPUT_NOPULL;
+    reg_mprj_io_3  = GPIO_MODE_USER_STD_INPUT_NOPULL;
+    reg_mprj_io_2  = GPIO_MODE_USER_STD_INPUT_NOPULL;
+    reg_mprj_io_1  = GPIO_MODE_USER_STD_INPUT_NOPULL;
+    reg_mprj_io_0  = GPIO_MODE_USER_STD_INPUT_NOPULL;
+
+    // Set clock to 64 kbaud and enable the UART.  It is important to do this
+    // before applying the configuration, or else the Tx line initializes as
+    // zero, which indicates the start of a byte to the receiver.
+
+    // reg_uart_clkdiv = 625;
+    reg_uart_enable = 1;
+
+    /* Apply configuration */
+    reg_mprj_xfer = 1;
+    while (reg_mprj_xfer == 1);
+
+    // // configure all LA as output
+	// reg_la0_oenb = reg_la0_iena = 0xFFFFFFFF;    // [31:0]
+	// reg_la1_oenb = reg_la1_iena = 0xFFFFFFFF;    // [63:32]
+	// reg_la2_oenb = reg_la2_iena = 0xFFFFFFFF;    // [95:64]
+	// reg_la3_oenb = reg_la3_iena = 0xFFFFFFFF;    // [127:96]
+
+    // reg_la0_data = 0x00000000;
+	// reg_la1_data = 0x00000000;
+	// reg_la2_data = 0x00000000;
+	// reg_la3_data = 0x00000000;
+
+    // Done configuring the IO
+    reg_mprj_cfg_cfg_done = 1;
+
+
+    bool skip_data_init = true;
+    if (!skip_data_init){
+        // use wb clk and rst
+        reg_mprj_cfg_mode = 1;
+        // enable debug mode to load/store data
+        reg_mprj_cfg_debug = 1;
+        
+        // query mem init
+        // num query
+        for (uint32_t i=0; i<2; i++){  // testing only
+        // for (uint32_t i=0; i<494; i++){
+            for (uint32_t j=0; j<2; j++){
+                reg_mprj_cfg_query[2 * i + j] = 2 * i + j;
+            }
+        }
+
+
+        // leaf mem init
+        // num leaf
+        for (uint32_t i=0; i<2; i++){  // testing only
+        // for (uint32_t i=0; i<63; i++){
+            // num patch
+            for (uint32_t j=0; j<8; j++){
+                for (uint32_t r=0; r<2; r++){
+                    reg_mprj_cfg_leaf[2 * 8 * i + 2 * j + r] = 2 * 8 * i + 2 * j + r;
+                }
+            }
+        }
+
+
+        // internal node init
+
+
+        // disable debug mode to release memory control
+        reg_mprj_cfg_mode = 0;
+        reg_mprj_cfg_debug = 0;
+    }
+
+
+    // // start fsm
+    // reg_mprj_cfg_fsm_done = 0;
+    // reg_mprj_cfg_fsm_start = 1;
+    // while(reg_mprj_cfg_fsm_done == 1);
+    // reg_mprj_cfg_fsm_done = 0;
+
+    
+    // print("hello world\n");
+    bool test_pass = true;
+
+    bool debug_mem = true;
+    if (debug_mem){
+        while(reg_mprj_cfg_load_done==0);
+
+        // enable debug mode to load/store data
+        reg_mprj_cfg_mode = 1;
+        reg_mprj_cfg_debug = 1;
+
+        // query mem read
+        // num query
+        // for (uint32_t i=0; i<2; i++){  // testing only
+        for (uint32_t i=0; i<494; i++){
+            uint32_t data0 = reg_mprj_cfg_query[2 * i + 0];
+            uint32_t data1 = reg_mprj_cfg_query[2 * i + 1];
+            uint64_t data64b = ((uint64_t)data1 << 32) | data0;
+            uint64_t mask_11b = 2047;
+            for (uint32_t m=0; m<5; m++){
+                uint64_t data_11b = data64b & mask_11b;
+                data_11b = data_11b >> (m * 11);
+                uint64_t expected_data_11b = (5 * i + m) & 0x7FF;
+                if (data_11b != expected_data_11b) test_pass = false;
+                mask_11b = mask_11b << 11;
+            }
+        }
+
+        // // leaf mem read
+        // // num leaf
+        // // for (uint32_t i=0; i<2; i++){  // testing only
+        // for (uint32_t i=0; i<63; i++){
+        //     // num patch
+        //     for (uint32_t j=0; j<8; j++){
+        //         uint32_t data0 = reg_mprj_cfg_leaf[2 * i + 0];
+        //         uint32_t data1 = reg_mprj_cfg_leaf[2 * i + 1];
+        //         uint64_t data64b = ((uint64_t)data1 << 32) | data0;
+        //         uint64_t mask_11b = 2047;
+        //         for (uint32_t m=0; m<6; m++){
+        //             uint64_t data_11b = data64b & mask_11b;
+        //             data_11b = data_11b >> (m * 11);
+        //             uint64_t expected_data_11b = (6 * 8 * i + 6 * j + m);
+        //             if (m == 5)
+        //                 expected_data_11b = expected_data_11b & 0x1FF;
+        //             else
+        //                 expected_data_11b = expected_data_11b & 0x7FF;
+        //             if (data_11b != expected_data_11b) test_pass = false;
+        //             mask_11b = mask_11b << 11;
+        //         }
+        //     }
+        // }
+
+        // // internal node tree read
+        // for (uint32_t i=0; i<63; i++){
+        //     uint32_t data = reg_mprj_cfg_node[i];
+
+        //     // 3 bit index
+        //     uint32_t data_idx3b = data & 0x07;
+        //     uint32_t expected_data = (2 * i) & 0x07;
+        //     if (data_idx3b != expected_data) {
+        //         reg_mprj_cfg_cfg_done = 1;
+        //         test_pass = false;
+        //     }
+
+        //     // 11 bit median
+        //     uint32_t data_11b = (data & 0x03FF800) >> 11;
+        //     // median is in bits [21:11]
+        //     expected_data = (2 * i + 1) & 0x7FF;
+        //     if (data_11b != expected_data) {
+        //         reg_mprj_cfg_cfg_done = 1;
+        //         test_pass = false;
+        //     }
+
+        //     // uint32_t mask_11b = 0x7FF;
+        //     // for (uint32_t m=0; m<2; m++){
+        //     //     uint32_t data_11b = data & mask_11b;
+        //     //     uint32_t expected_data_11b = (2 * i + m) & 0x7FF;
+        //     //     data_11b = data_11b >> (m * 11);
+        //     //     if (data_11b != expected_data_11b) {
+        //     //         reg_mprj_cfg_cfg_done = 1;
+        //     //         test_pass = false;
+        //     //     }
+        //     //     mask_11b = mask_11b << 11;
+        //     // }
+        // }
+
+        // disable debug mode to release memory control
+        reg_mprj_cfg_mode = 0;
+        reg_mprj_cfg_debug = 0;
+    }
+
+    // temporary hack
+    if (test_pass) reg_mprj_cfg_cfg_done = 0;
+
+    // tell ann_tb to stop
+    reg_mprj_cfg_done = 1;
+}
---
+++ verilog/dv/ann_dbg_mem/ann_dbg_mem_tb.v
@@ -0,0 +1,280 @@
+// SPDX-FileCopyrightText: 2020 Efabless Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// SPDX-License-Identifier: Apache-2.0
+
+`default_nettype none
+
+`timescale 1 ns / 1 ps
+
+module ann_dbg_mem_tb;
+    parameter DATA_WIDTH = 11;
+    parameter LEAF_SIZE = 8;
+    parameter PATCH_SIZE = 5;
+    parameter ROW_SIZE = 26;
+    parameter COL_SIZE = 19;
+    parameter NUM_QUERYS = ROW_SIZE * COL_SIZE;
+    parameter NUM_LEAVES = 64;
+    parameter NUM_NODES = NUM_LEAVES - 1;
+    parameter BLOCKING = 4;
+    parameter ADDR_WIDTH = $clog2(NUM_LEAVES);
+
+    reg clock;
+    reg RSTB;
+    reg CSB;
+    reg power1, power2;
+    reg power3, power4;
+
+    wire gpio;
+    wire [37:0] mprj_io;
+
+    reg                                 io_clk;
+    reg                                 io_rst_n;
+    reg                                 fsm_start;
+    wire                                fsm_done;
+    reg                                 send_best_arr;
+    reg                                 load_kdtree;
+    reg                                 in_fifo_wenq;
+    reg [10:0]                          in_fifo_wdata;
+    wire                                in_fifo_wfull_n;
+    reg                                 out_fifo_deq;
+    wire [10:0]                         out_fifo_rdata;
+    wire                                out_fifo_rempty_n;
+    wire                                wbs_done;
+    wire                                wbs_busy;
+    wire                                wbs_cfg_done;
+
+
+    assign mprj_io[0] = io_clk;
+    assign mprj_io[1] = io_rst_n;
+    assign mprj_io[2] = in_fifo_wenq;
+    assign mprj_io[13:3] = in_fifo_wdata;
+    assign mprj_io[14] = out_fifo_deq;
+    assign mprj_io[15] = fsm_start;
+    assign mprj_io[16] = send_best_arr;
+    assign mprj_io[17] = load_kdtree;
+    assign in_fifo_wfull_n = mprj_io[18];
+    assign out_fifo_rdata = mprj_io[29:19];
+    assign out_fifo_rempty_n = mprj_io[30];
+    assign fsm_done = mprj_io[31];
+    assign wbs_done = mprj_io[32];
+    assign wbs_busy = mprj_io[33];
+    assign wbs_cfg_done = mprj_io[34];
+
+    // External clock is used by default.  Make this artificially fast for the
+    // simulation.  Normally this would be a slow clock and the digital PLL
+    // would be the fast clock.
+
+    always #1 clock <= (clock === 1'b0);
+    always #5 io_clk <= (io_clk === 1'b0);
+
+    initial begin
+        clock = 0;
+        io_clk = 0;
+    end
+
+
+    integer x;
+    integer xi;
+    integer y;
+    integer addr;
+    real simtime;
+    real kdtreetime;
+    real querytime;
+    real fsmtime;
+    real outputtime;
+    integer i;
+    integer px;
+    integer agg;
+
+    initial begin
+        $timeformat(-9, 2, "ns", 20);
+        $dumpfile("ann_dbg_mem.vcd");
+        $dumpvars(0, ann_dbg_mem_tb);
+
+        
+        fsm_start = 0;
+        send_best_arr = 0;
+        load_kdtree = 0;
+        io_rst_n = 1'b1;
+        in_fifo_wenq = 0;
+        in_fifo_wdata = 11'd0;
+        out_fifo_deq = 0;
+
+
+        // wait for mgmt soc to finish configuring the io ports
+        wait(wbs_cfg_done);
+
+
+        // reset accelerator
+        #100
+        io_rst_n = 0;
+        #20
+        io_rst_n = 1'b1;
+
+        // start load kd tree internal nodes and leaves
+        @(negedge io_clk) load_kdtree = 1'b1;
+        simtime = $realtime;
+        $display("[T=%0t] Start sending KD tree internal nodes and leaves", $realtime);
+        @(negedge io_clk) load_kdtree = 1'b0;
+
+        // send internal nodes, 2 lines per node
+        // index
+        // median
+        for(i=0; i<NUM_NODES*2; i=i+1) begin
+            @(negedge io_clk)
+            in_fifo_wenq = 1'b1;
+            in_fifo_wdata = i;
+        end
+        @(negedge io_clk)
+        in_fifo_wenq = 0;
+        in_fifo_wdata = 11'd0;
+
+        // send leaves, 6*8 lines per leaf
+        // 8 patches per leaf
+        // each patch has 5 lines of data
+        // and 1 line of patch index in the original image (for reconstruction)
+        for(i=0; i<NUM_LEAVES*6*8; i=i+1) begin
+            @(negedge io_clk)
+            in_fifo_wenq = 1'b1;
+            in_fifo_wdata = i;
+        end
+        @(negedge io_clk)
+        in_fifo_wenq = 0;
+        in_fifo_wdata = 11'd0;
+        $display("[T=%0t] Finished sending KD tree internal nodes and leaves", $realtime);
+        kdtreetime = $realtime - simtime;
+        
+        $display("[T=%0t] Start sending queries", $realtime);
+        simtime = $realtime;
+        // send query patches, 5 lines per query patch
+        // each patch has 5 lines of data
+        for(i=0; i<NUM_QUERYS*5; i=i+1) begin
+            @(negedge io_clk)
+            in_fifo_wenq = 1'b1;
+            in_fifo_wdata = i;
+        end
+        @(negedge io_clk)
+        in_fifo_wenq = 0;
+        in_fifo_wdata = 11'd0;
+        $display("[T=%0t] Finished sending queries", $realtime);
+        querytime = $realtime - simtime;
+
+
+        $display("===============Runtime Summary===============");
+        $display("KD tree: %t", kdtreetime);
+        $display("Query patches: %t", querytime);
+    end
+
+    // initial begin
+    //     // Repeat cycles of 1000 clock edges as needed to complete testbench
+    //     repeat (50) begin
+    //         repeat (1000) @(posedge clock);
+    //         $display("+1000 cycles");
+    //     end
+    //     $display("%c[1;31m",27);
+    //     $display("%c[0m",27);
+    //     $finish;
+    // end
+
+    initial begin
+        $display("Monitor: MPRJ-Logic WB Started");
+        wait(wbs_done == 1);
+        if (wbs_cfg_done == 0) begin
+            `ifdef GL
+                $display("Monitor: Mega-Project WB (GL) Passed");
+            `else
+                $display("Monitor: Mega-Project WB (RTL) Passed");
+            `endif
+        end
+        else begin
+            `ifdef GL
+                $display ("Monitor: Test Mega-Project WB Port (GL) Failed");
+            `else
+                $display ("Monitor: Test Mega-Project WB Port (RTL) Failed");
+            `endif
+        end
+        $finish;
+    end
+
+    initial begin
+        RSTB <= 1'b0;
+        CSB  <= 1'b1;		// Force CSB high
+        #2000;
+        RSTB <= 1'b1;	    	// Release reset
+        #100000;
+        CSB = 1'b0;		// CSB can be released
+    end
+
+    initial begin		// Power-up sequence
+        power1 <= 1'b0;
+        power2 <= 1'b0;
+        #200;
+        power1 <= 1'b1;
+        #200;
+        power2 <= 1'b1;
+    end
+
+    wire flash_csb;
+    wire flash_clk;
+    wire flash_io0;
+    wire flash_io1;
+
+    wire VDD3V3 = power1;
+    wire VDD1V8 = power2;
+    wire USER_VDD3V3 = power3;
+    wire USER_VDD1V8 = power4;
+    wire VSS = 1'b0;
+
+    caravel uut (
+        .vddio	  (VDD3V3),
+        .vddio_2  (VDD3V3),
+        .vssio	  (VSS),
+        .vssio_2  (VSS),
+        .vdda	  (VDD3V3),
+        .vssa	  (VSS),
+        .vccd	  (VDD1V8),
+        .vssd	  (VSS),
+        .vdda1    (VDD3V3),
+        .vdda1_2  (VDD3V3),
+        .vdda2    (VDD3V3),
+        .vssa1	  (VSS),
+        .vssa1_2  (VSS),
+        .vssa2	  (VSS),
+        .vccd1	  (VDD1V8),
+        .vccd2	  (VDD1V8),
+        .vssd1	  (VSS),
+        .vssd2	  (VSS),
+        .clock    (clock),
+        .gpio     (gpio),
+        .mprj_io  (mprj_io),
+        .flash_csb(flash_csb),
+        .flash_clk(flash_clk),
+        .flash_io0(flash_io0),
+        .flash_io1(flash_io1),
+        .resetb	  (RSTB)
+    );
+
+    spiflash #(
+        .FILENAME("ann_dbg_mem.hex")
+    ) spiflash (
+        .csb(flash_csb),
+        .clk(flash_clk),
+        .io0(flash_io0),
+        .io1(flash_io1),
+        .io2(),			// not used
+        .io3()			// not used
+    );
+
+endmodule
+`default_nettype wire
